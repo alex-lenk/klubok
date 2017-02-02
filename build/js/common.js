e5cf673e51c8bcef859c2a6a158fc203ccf2b815
artlenk.ru@gmail.com <df5ukoi7ht>
2017-02-02 15:58:27 +0300

    Published folder build

---
+++ build/js/common.js
@@ -0,0 +1,86 @@
+function activeItemLogic(itemsSelector, activeClassName){
+	var items = $(itemsSelector);
+	items.on('click', function(){
+		if ( $(this).hasClass(activeClassName) == false ){
+			items.filter('.'+activeClassName).removeClass(activeClassName);
+			$(this).addClass(activeClassName);
+		}
+	});
+}
+
+/*function bumpFooter(){
+	$('.panel').css('min-height', $('body').height() - $('footer').height() - 20 +'px');
+}*/
+
+var headerModule = {
+	profileMinWidth: 0, 
+	profileInfoMinwidth: 0,
+	profileImgMax: 260,
+	profileImgMin: 100,
+	init: function() {
+		$(".name").css('white-space', 'nowrap');
+		$(".name").css('display', 'block');
+		this.profileMinWidth = $('.profile-info').width() + $('.profile-actions').width();
+		this.profileInfoMinwidth = $(".profile-info").width() + 20;
+		$(".name").removeAttr( 'style' );
+		this.resizeHeader();
+	},
+	resizeHeader: function() {
+		var windowWidth = $(window).width();
+		$(".header-middle").removeClass("header-middle-small");
+		$('.header-media-content').removeAttr( 'style' );
+		$(".header-media").removeClass('header-media-small');
+
+		if (  windowWidth < this.profileInfoMinwidth + this.profileImgMin ) { 
+			$(".header-media").addClass('header-media-small');
+			$(".header-middle").addClass("header-middle-small");
+		} else if ( windowWidth < this.profileMinWidth + this.profileImgMax + 30 ) {
+			$(".header-middle").addClass("header-middle-small");
+			$('.header-media-content').css('min-width', this.profileInfoMinwidth + 'px');
+		} 
+		this.updateImageBorderWidth();
+	},
+	updateImageBorderWidth: function() {
+		var profileImg = $(".profile-img");
+		var uploadProfileImg = $(".upload-profile-img");
+		var borderWidth = ~~( ( profileImg.width() -45 ) / 35 ) + 1;
+
+		profileImg.removeAttr( 'style' );
+		uploadProfileImg.removeAttr( 'style' );
+
+		profileImg.css('border', borderWidth+'px solid #fff');
+		uploadProfileImg.css('margin', borderWidth+'px');
+	}
+};
+
+
+$(window).load(function() {
+
+	$('a').on('click', function(e){
+		e.preventDefault();
+	});
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+
+
+	$('[data-toggle="tooltip"]').tooltip({
+		'delay': { show: 300, hide: 50 }
+	});
+
+	$(".login-options__item").click(function(){
+		$(this).toggleClass("login-options-active");
+	});
+
+/*	activeItemLogic('.language>a', 'active-language');*/
+
+	headerModule.init();
+/*	bumpFooter();*/
+
+/*	$(window).on("resize", function() {
+		headerModule.resizeHeader();
+		bumpFooter();
+	});*/
+
+});
